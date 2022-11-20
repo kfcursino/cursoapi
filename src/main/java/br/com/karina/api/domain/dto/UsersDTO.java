323f441dd28896070e2f004e314137c9ec8cd0d2
kfcursino <kfcursino@gmail.com>
2022-11-20 19:06:53 -0300

    classe user dto

---
+++ src/main/java/br/com/karina/api/domain/dto/UsersDTO.java
@@ -0,0 +1,17 @@
+package br.com.karina.api.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsersDTO {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
