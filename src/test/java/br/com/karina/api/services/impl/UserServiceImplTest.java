9e84767f611fc77ebe17fafe609ca63d69d85f9d
kfcursino <kfcursino@gmail.com>
2022-11-27 19:56:38 -0300

    teste para classe user service metodo create com sucesso

--- src/test/java/br/com/karina/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/karina/api/services/impl/UserServiceImplTest.java
@@ -18,6 +18,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -91,8 +92,20 @@ class UserServiceImplTest {
     }
 
     @Test
-    void create() {
+    void whenCreateThenReturnSuccess() {
+        when(repository.save(any())).thenReturn(user);
+
+        Users response = service.create(userDTO);
+
+        assertNotNull(response);
+        assertEquals(Users.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
     }
+
+
 
     @Test
     void update() {
