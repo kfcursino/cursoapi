98ae3af4773e750cb1846db2ddfa8db33af22ef1
kfcursino <kfcursino@gmail.com>
2022-11-26 21:54:40 -0300

    teste para classe principal

--- src/test/java/br/com/karina/api/ApiApplicationTests.java
+++ src/test/java/br/com/karina/api/ApiApplicationTests.java
@@ -1,5 +1,6 @@
 package br.com.karina.api;
 
+import org.apiguardian.api.API;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		ApiApplication.main(new String[] {});
 	}
 
 }
