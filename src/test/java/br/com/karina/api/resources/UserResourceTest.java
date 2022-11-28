b48f47eae7a72315cf4039362c46f70e34567e9f
kfcursino <kfcursino@gmail.com>
2022-11-27 21:13:03 -0300

    teste para endpoint create da classe user resource implementado

--- src/test/java/br/com/karina/api/resources/UserResourceTest.java
+++ src/test/java/br/com/karina/api/resources/UserResourceTest.java
@@ -20,6 +20,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
@@ -91,7 +92,15 @@ class UserResourceTest {
     }
 
     @Test
-    void create() {
+    void whenCreateThenReturnCreated() {
+        when(service.create(any())).thenReturn(user);
+
+        ResponseEntity<UsersDTO> response = resource.create(userDTO);
+
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getHeaders().get("Location"));
+        assertNull(response.getBody());
     }
 
     @Test
